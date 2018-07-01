74fc46d7ec0cf2509e0735dab330d74f0872588b
dankelley <kelley.dan@gmail.com>
2018-07-01 11:06:26 -0300

    update vignette

--- inst/doc/whalestrike.R
+++ inst/doc/whalestrike.R
@@ -5,8 +5,16 @@ knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 library(whalestrike)
 t <- seq(0, 0.5, length.out=500)
 state <- c(xs=-1.5, vs=5, xw=0, vw=0)
-parms <- parameters(ms=20e3, Ss=15*pi*3, B=3, D=1.5,
-              lw=10, Sw=10*2*pi*3)
+
+ms <- 20e3                             # ship mass
+Ss <- 11.73*(2*1.58+4.63)              # ship wetted area (max)
+impactWidth <- 4.6                     # impact width
+impactHeight <- 1.6                    # impact height
+lw <- 10                               # whale length
+mw <- whaleMassFromLength(lw)          # whale mass
+Sw <- whaleAreaFromLength(lw,"wetted") # whale area
+parms <- parameters(ms=ms, Ss=Ss, impactWidth=impactWidth, impactHeight=impactHeight,
+                    lw=lw, mw=mw, Sw=Sw)
 sol <- strike(t, state, parms)
 par(mfcol=c(3, 3), mar=c(2, 3, 0.5, 0.5), mgp=c(2, 0.7, 0), cex=0.7)
 plot(sol)
@@ -18,8 +26,9 @@ state <- c(xs=-1.5, vs=5, xw=0, vw=0)
 beta <- seq(0.1, 0.3, length.out=100)
 maxAccel <- rep(NA, length(beta))
 for (i in seq_along(beta)) {
-    parms <- parameters(ms=20e3, Ss=15*pi*3, B=3, D=1.5,
-                        lw=10, Sw=10*2*pi*3, delta=0.02, Es=2e7, theta=45,
+    parms <- parameters(ms=ms, Ss=Ss, impactWidth=impactWidth, impactHeight=impactHeight,
+                        lw=lw, mw=mw, Sw=Sw,
+                        delta=0.02, Es=2e7, theta=45,
                         Eb=1e6, beta=beta[i])
     sol <- strike(t, state, parms)
     maxAccel[i] <- max(abs(diff(sol$vw))) / (t[2] - t[1])
@@ -31,21 +40,23 @@ library(whalestrike)
 t <- seq(0, 1, length.out=500)
 ## Hint: making x and y of different lengths, to avoid row,col
 ## versus i,j confusion.
-beta <- seq(0.1, 0.4, length.out=19)
-speedK <- seq(2, 15, length.out=20) # in knots
+beta <- seq(0.1, 0.4, length.out=9)
+speedK <- seq(2, 15, length.out=10) # in knots
 speed <- 0.5144 * speedK
-maxAccel <- matrix(NA, nrow=length(speed), ncol=length(beta))
+maxStrain <- matrix(NA, nrow=length(speed), ncol=length(beta))
 for (i in seq_along(beta)) {
     for (j in seq_along(speed)) {
-        cat(i, ' ', j, '\n')
         state <- c(xs=-1.5, vs=speed[j], xw=0, vw=0)
-        parms <- parameters(ms=20e3, Ss=15*pi*3, B=3, D=1.5,
-                            lw=10, Sw=10*2*pi*3, delta=0.02, Es=2e7, theta=45,
+        parms <- parameters(ms=ms, Ss=Ss, impactWidth=impactWidth, impactHeight=impactHeight,
+                            lw=lw, mw=mw, Sw=Sw,
+                            delta=0.02, Es=2e7, theta=45,
                             Eb=1e6, beta=beta[i])
         sol <- strike(t, state, parms)
-        maxAccel[j, i] <- max(abs(diff(sol$vw))) / (t[2] - t[1])
+        maxStrain[j, i] <- max(sol$WCF$strain)
     }
 }
-contour(speedK, beta, maxAccel, xlab="Speed [knots]", ylab="Blubber thickness [m]")
-mtext("Contours of acceleration [m/s^2]", side=3)
+contour(speedK, beta, maxStrain, level=seq(0, 0.3, 0.05),
+        xlab="Speed [knots]", ylab="Blubber thickness [m]")
+contour(speedK, beta, maxStrain, level=seq(0.3, 1, 0.05), lwd=3, add=TRUE)
+mtext("Contours of max strain (bold if unsafe)", side=3)
 
