7dae7b83668989615a2c30657bb211afb049343a
dankelley <kelley.dan@gmail.com>
2018-07-19 10:43:22 -0300

    fix raymond2007 digitization error

--- create_data/raymond2007/raymond2007_fig2_13.R
+++ create_data/raymond2007/raymond2007_fig2_13.R
@@ -1,5 +1,6 @@
-d <- read.table("raymond2007_fig2_13.dat", skip=7, header=TRUE)
+d <- read.csv("raymond2007_fig2_13_imager.dat", skip=7, header=TRUE)
 raymond2007 <- list(strain=d$strain, stress=d$stress)
 save(raymond2007, file="raymond2007.rda")
 tools::resaveRdaFiles("raymond2007.rda", compress="auto")
+plot(raymond2007$strain, raymond2007$stress, type="o")
 
